beda43b8fe81937a2445bd27d06f2548a314924d
Marlon <maestupinan2@poligran.edu.co>
2020-06-19 03:47:15 -0500

    File picker

--- src/app/problem/problem-detail/problem-detail.component.ts
+++ src/app/problem/problem-detail/problem-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Problem } from 'src/app/model/problem';
 import { ActivatedRoute } from '@angular/router';
 import { ProblemService } from 'src/app/services/problem/problem.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-problem-detail',
@@ -12,10 +13,27 @@ export class ProblemDetailComponent implements OnInit {
 
   @Input() problem: Problem;
 
+  form: FormGroup;
+  fileToUpload: File = null;
+
+  text: string = "Agregar Archivo";
+
+
   constructor(private route: ActivatedRoute,
-    private problemService: ProblemService) { }
+    private problemService: ProblemService,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      file: null
+    });
+
+    this.form.get("file").valueChanges.subscribe(value =>{
+      this.text = value.split("\\").pop();
+      if(this.text.length == 0)
+        this.text = "Agregar Archivo";
+
+    })
     this.getProblem();
   }
 
@@ -32,5 +50,20 @@ export class ProblemDetailComponent implements OnInit {
 
   }
 
+  onSubmit(){
+      console.log(this.fileToUpload);
+      this.fileToUpload.text().then(value =>{
+        console.log(value);
+      })
+      
+  }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+    if(!(["cpp", "java", "py", "c"].includes(this.fileToUpload.name.split(".").pop()))){
+      this.fileToUpload = null;
+      this.text = "Agregar Archivo";
+    }
+  }
 
 }
