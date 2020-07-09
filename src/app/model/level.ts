fbb7f227700847572cee5f84c859fa2f0c9cad6f
Andrés Osorio <gd_andres98@hotmail.com>
2020-07-09 18:57:26 -0500

    modelo Level

--- src/app/model/level.ts
+++ src/app/model/level.ts
@@ -1,4 +1,10 @@
-export class Level{
-    public id: number
+import { Contest } from './contest';
+import { Article } from './article';
 
+export class Level{
+    public id: number;
+    public name: string;
+    public logo: string;
+    public problems: Contest;
+    public articles: Article[];
 }
